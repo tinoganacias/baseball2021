ec05c34d3a66cf8f0d98c1c378286add15a55cca
Tino Ganacias <84544540+tinoganacias@users.noreply.github.com>
2022-01-13 13:23:19 -0800

    Add files via upload
    testing baseball_db upload

---
+++ firstbase_metric_schema.sql
@@ -0,0 +1,19 @@
+USE baseball_db;
+
+CREATE TABLE firstbase_metric (
+  player_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(100) DEFAULT NULL,
+  last_name VARCHAR(100) DEFAULT NULL,
+  wRC INT DEFAULT NULL,
+  BB_percent DECIMAL(3,2) DEFAULT NULL,
+  K_percent DECIMAL(3,2) DEFAULT NULL,
+  barrel_p DECIMAL(3,2) DEFAULT NULL,
+  hardhit_p DECIMAL(3,2) DEFAULT NULL,
+  max_exit int DEFAULT NULL,
+  avg_EV int DEFAULT NULL,
+  zone_contact DECIMAL(3,2) DEFAULT NULL,
+  chase decimal(3,2) DEFAULT NULL,
+  BABIP decimal(4,3) DEFAULT NULL
+  
+  
+);
---
+++ firstbase_seeds.sql
@@ -0,0 +1,42 @@
+INSERT INTO firstbase (
+    player_id,first_name,last_name,team,age
+)
+
+VALUES 
+(1,'Vladimir','Guerrero Jr','Toronto',22),
+(2,'Freddie','Freeman','Atlanta',31),
+(3,'Paul','Goldschmidt','St. Louis',33),
+(4,'Matt','Olson','Oakland',27),
+(5,'Jose','Abreu','Chicago(A)',34),
+(6,'Carlos','Santana','Kansas City',35),
+(7,'Nathaniel','Lowe','Texas',25),
+(8,'Pete','Alonso','New York(N)',26),
+(9,'Trey','Mancini','Baltimore',29),
+(10,'Yuli','Gurriel','Houston',37),
+(11,'Ryan','Mountcastle','Baltimore',24),
+(12,'Jared','Walsh','Los Angeles(A)',27),
+(13,'Anthony','Rizzo','New York(A)',31),
+(14,'Josh','Bell','Washington',28),
+(15,'Eric','Hosmer','San Diego',31),
+(16,'CJ','Cron','Colorado',31),
+(17,'Pavin','Smith','Arizona',25),
+(18,'Joey','Votto','Cincinnati',37),
+(19,'Miguel','Sano','Minnesota',28),
+(20,'Miguel','Cabrera','Detroit',38),
+(21,'Jesus','Aguilar','Miami',31),
+(22,'Bobby','Dalbec','Boston',26),
+(23,'Christian','Walker','Arizona',30),
+(24,'Rhys','Hoskins','Philadelphia',28),
+(25,'Brandon','Belt','San Francisco',33),
+(26,'Brad','Miller','Philadelphia',31),
+(27,'Colin','Moran','Pittsburgh',28),
+(28,'Rowdy','Tellez','Milwaukee',26),
+(29,'Darin','Ruf','San Francisco',34),
+(30,'Ji Man','Choi','Tampa Bay',30),
+(31,'Bobby','Bradley','Cleveland',25),
+(32,'Yoshi','Tsutsugo','Pittsburgh',29),
+(33,'Frank','Schwindel','Chicago(N)',29),
+(34,'Daniel','Vogelbach','Milwaukee',28),
+(35,'Garrett','Cooper','Miami',30);
+
+SELECT * FROM firstbase;
