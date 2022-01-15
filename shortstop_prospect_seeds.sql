13c9834f50e7e0a3db679e68d05156a8f8f95858
Tino Ganacias <84544540+tinoganacias@users.noreply.github.com>
2022-01-14 16:10:55 -0800

    Add files via upload

---
+++ shortstop_prospect_seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO shortstop_prospect (
+    player_id,first_name,last_name,team,age,mlb
+)
+
+VALUES 
+
+(1,'Bobby','Witt','Kansas City',21,1),
+(2,'Marco','Luciano','San Francisco',20,2),
+(3,'CJ','Abrams','San Diego',21,3),
+(4,'Marcelo','Mayer','Boston',18,4),
+(5,'Noelvi','Marte','Seattle',20,5),
+(6,'Jordan','Lawlar','Arizona',19,6),
+(7,'Anthony','Volpe','New York(A)',20,7),
+(8,'Kahlil','Watson','Miami',18,8),
+(9,'Royce','Lewis','Minnesota',22,9),
+(10,'Orelvis','Martinez','Toronto',20,10);
+
+SELECT * FROM shortstop_prospect;
---
+++ shortstop_seeds.sql
@@ -0,0 +1,45 @@
+INSERT INTO shortstop (
+    player_id,first_name,last_name,team,age
+
+)
+
+VALUES 
+ 
+ (1,'Bo','Bichette','Toronto',23),
+ (2,'JP','Crawford','Seattle',26),
+ (3,'Isiah','Kiner Falefa','Texas',26),
+ (4,'Dansby','Swanson','Atlanta',27),
+ (5,'Carlos','Correa','Houston',26),
+ (6,'Xander','Bogaerts','Boston',28),
+ (7,'Trevor','Story','Colorado',28),
+ (8,'Amed','Rosario','Cleveland',25),
+ (9,'Luis','Urias','Milwaukee',24),
+ (10,'Nicky','Lopez','Kansas City',26),
+ (11,'Willy','Adames','Milwaukee',25),
+ (12,'Kevin','Newman','Pittsburgh',27),
+ (13,'Tim','Anderson','Chicago(A)',28),
+ (14,'Brandon','Crawford','San Francisco',34),
+ (15,'Fernando','Tatis Jr','San Diego',22),
+ (16,'Elvis','Andrus','Oakland',32),
+ (17,'Miguel','Rojas','Miami',32),
+ (18,'Kyle','Farmer','Cincinnati',30),
+ (19,'Francisco','Lindor','New York(N)',27),
+ (20,'Gleyber','Torres','New York(A)',24),
+ (21,'Jose','Iglesias','Boston',31),
+ (22,'Nick','Ahmed','Arizona',31),
+ (23,'Andrelton','Simmons','Minnesota',31),
+ (24,'Corey','Seager','Texas',27),
+ (25,'Didi','Gregorius','Philadelphia',31),
+ (26,'Paul','DeJong','St.Louis',27),
+ (27,'Freddy','Galvis','Baltimore',31),
+ (28,'Edmundo','Sosa','St.Louis',25),
+ (29,'Niko','Goodrum','Detroit',29),
+ (30,'Wander','Franco','Tampa Bay',20),
+ (31,'Ramon','Urias','Baltimore',27),
+ (32,'Andres','Gimenez','Cleveland',22),
+ (33,'Jorge','Mateo','Baltimore',26),
+ (34,'Luis','Rengifo','Los Angeles(A)',24),
+ (35,'Mauricio','Dubon','San Francisco',26),
+ (36,'Taylor','Walls','Tampa Bay',24);
+
+ SELECT * FROM shortstop;
