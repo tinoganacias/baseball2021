c70765cd0e6d234d1addbf960314359096d968eb
Tino Ganacias <84544540+tinoganacias@users.noreply.github.com>
2022-01-14 16:13:45 -0800

    Add files via upload
    catcher, fantasy, metric, prospects

---
+++ catcher_prospect_seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO catcher_prospect (
+    player_id,first_name,last_name,team,age,mlb
+)
+
+VALUES
+
+(1,'Adley','Rutschman','Baltimore',23,1),
+(2,'Francisco','Alvarez','New York(N)',20,2),
+(3,'Joey','Bart','San Francisco',24,3),
+(4,'Henry','Davis','Pittsburgh',22,4),
+(5,'Diego','Cartaya','Los Angeles(N)',20,5),
+(6,'Gabriel','Moreno','Toronto',21,6),
+(7,'Luis','Campusano','San Diego',23,7),
+(8,'Tyler','Soderstrom','Oakland',20,8),
+(9,'MJ','Melendez','Kansas City',22,9),
+(10,'Shea','Langeliers','Atlanta',24,10);
+
+SELECT * FROM catcher_prospect;
---
+++ catcher_seeds.sql
@@ -0,0 +1,44 @@
+INSERT INTO catcher (
+    player_id,first_name,last_name,team,age
+)
+
+VALUES 
+
+(1,'Salvador','Perez','KC',31),
+(2,'JT','Realmuto','Philadelphia',30),
+(3,'Will','Smith','LA Dodgers',26),
+(4,'Christian','Vasquez','Boston',31),
+(5,'Willson','Contreras','Chicago(N)',29),
+(6,'Yadier','Molina','St.Louis',39),
+(7,'Buster','Posey','San Francisco',34),
+(8,'Sean','Murphy','Oakland',27),
+(9,'Omar','Narvaez','Milwaukee',29),
+(10,'Gary','Sanchez','New York(A)',28),
+(11,'Jacob','Stallings','Pittsburgh',31),
+(12,'Martin','Maldonado','Houston',35),
+(13,'Pedro','Severino','Baltimore',28),
+(14,'James','McCann','New York(N)',31),
+(15,'Tyler','Stephenson','Cincinnati',24),
+(16,'Eric','Haase','Detroit',28),
+(17,'Luis','Torrens','Seattle',25),
+(18,'Yasmani','Grandal','Chicago(A)',32),
+(19,'Mike','Zunino','Tampa Bay',30),
+(20,'Yan','Gomes','Washington',33),
+(21,'Elias','Diaz','Colorado',30),
+(22,'Carson','Kelly','Arizona',26),
+(23,'Victor','Caratini','San Diego',27),
+(24,'Tom','Murphy','Seattle',30),
+(25,'Max','Stassi','Los Angeles(A)',30),
+(26,'Daulton','Varsho','Arizona',24),
+(27,'Austin','Hedges','Cleveland',28),
+(28,'Jorge','Alfaro','Miami',28),
+(29,'Jose','Trevino','Texas',28),
+(30,'Francisco','Mejia','Tampa Bay',25),
+(31,'Mitch','Garver','Minnesota',30),
+(32,'Travis','dArnaud','Atlanta',32),
+(33,'Danny','Jansen','Toronto',26),
+(34,'Austin','Nola','San Diego',31),
+(35,'Alejandro','Kirk','Toronto',22),
+(36,'Tucker','Barnhart','Detroit',30);
+
+SELECT * FROM catcher;
