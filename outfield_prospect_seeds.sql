adb4348843741ac75e2d063801555d6a494d773e
Tino Ganacias <84544540+tinoganacias@users.noreply.github.com>
2022-01-14 16:16:43 -0800

    Add files via upload
    outfield, fantasy stats, metrics, prospects

---
+++ outfield_prospect_seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO outfield_prospect (
+    player_id,first_name,last_name,team,age,mlb
+)
+ VALUES 
+ 
+ (1,'Julio','Rodriguez','Seattle',20,1),
+ (2,'Riley','Greene','Detroit',21,2),
+ (3,'Brennan','Davis','Chicago(N)',22,3),
+ (4,'Jasson','Dominguez','New York(A)',18,4),
+ (5,'Corbin','Carroll','Arizona',21,5),
+ (6,'Vidal','Brujan','Tampa Bay',23,6),
+ (7,'Jarren','Duran','Boston',25,7),
+ (8,'Austin','Martin','Minnesota',22,8),
+ (9,'Cristian','Pache','Atlanta',23,9),
+ (10,'Alek','Thomas','Arizona',21,10);
+
+ SELECT * FROM outfield_prospect;
---
+++ outfield_schema.sql
@@ -0,0 +1,9 @@
+USE baseball_db;
+
+CREATE TABLE outfield (
+  player_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name varchar(100) DEFAULT NULL,
+  last_name varchar(100) DEFAULT NULL,
+  team varchar(100) DEFAULT NULL,
+  age int DEFAULT NULL
+ ); 
